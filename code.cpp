bd77ec2a1af543aa0fe72063447f89a66effa8f3
RabiulHasanReza <pikachud110@gmail.com>
2024-01-26 03:49:52 +0600

    progess

--- code.cpp
+++ code.cpp
@@ -1,30 +1,66 @@
 #include "iGraphics.h"
-
+#include<time.h>
 bool musicOn = true;
 int screen = 0;
+
+struct puzzle{
+	int x;
+	int y;
+};
+
+struct puzzle pic[9];
+
+char puzzlePices[9][20]={"image1x1.bmp","image1x2.bmp","image1x3.bmp","image2x1.bmp","image2x2.bmp","image2x3.bmp",
+ "image3x1.bmp","image3x2.bmp","image3x3.bmp"};
+
+ int cordinatesX[3]={0,167,167*2},cordinatesY[3]={0,167,167*2};
+ int indexX=167;
+ int indexY=167;
+ int x;
+ int y;
+
 
 void iDraw()
 {
 	iClear();
 	// place your drawing codes here
 	if (screen == 0)
-	{
+	{   
 
 		iShowBMP(0, 0, "Home page.bmp");
 		iShowBMP2(30, 300, "button 1.bmp", 0);
 		iShowBMP2(30, 200, "button 2.bmp", 0);
 		iShowBMP2(30, 100, "button 3.bmp", 0);
+		iShowBMP2(580,20,"info button.bmp",0);
 	}
 	if (screen == 1)
 	{
-		iShowBMP(0, 0, "pikachuNew.bmp");
+		iShowBMP(0, 0, "screen2.bmp");
+		iShowBMP(550,300,"preview.bmp");
+		int count = 0;
+		for (int i = 0; i<=500; i+=167)
+		{
+			for (int j = 0; j<=500; j+=167)
+			{
+				// if (i == 0 && j == 0)    continue;
+				// else
+				// 
+				srand(time(NULL));
+					iShowBMP(i,j, puzzlePices[rand()%9+1]);
+					count++;
+				//}
+			}
+		}
 		iShowBMP2(550, 30, "button6.bmp", 0);
-
 	}
+
+		//iShowBMP(0,0,"pokemon.bmp");
+		
+
 	if (screen == 2)
 	{
 
-		iShowBMP(0, 0, "screen2.bmp");
+		iShowBMP(0, 0, "pikachuNew.bmp");
 		iShowBMP2(50, 400, "button4.bmp", 0);
 		iShowBMP2(550, 400, "button5.bmp", 0);
 		iShowBMP2(550, 30, "button6.bmp", 0);
@@ -34,8 +70,17 @@ void iDraw()
 		iShowBMP(0, 0, "screen3.bmp");
 		iShowBMP2(550, 30, "button6.bmp", 0);
 	}
+	if(screen == 4){
+         iShowBMP(0,0,"screen4.bmp");
+		 iText(30, 120, "Use Left click to move a picture .The picture you click will be swapped", GLUT_BITMAP_TIMES_ROMAN_24);
+		iText(30, 100, "with the blank pic.", GLUT_BITMAP_TIMES_ROMAN_24);
+		iText(30, 20, "You have \"30 SECONDS \" time to match it.", GLUT_BITMAP_TIMES_ROMAN_24);
+		iSetColor(0, 0, 0);
+		iText(30, 50, "Press o to mute sound and m to unmute.", GLUT_BITMAP_TIMES_ROMAN_24);
+		iShowBMP2(550, 400, "button6.bmp", 0);
+	}
 
-	iText(10, 10, "Use cursors to move the picture.");
+	
 }
 
 /*
@@ -69,6 +114,10 @@ void iMouse(int button, int state, int mx, int my)
 			{
 				screen = 3;
 			}
+			if(mx>=580 && mx <= 780 && my >= 20 && my <= 88)
+			{
+				screen = 4;
+			}
 		}
 		
 		if(screen==1)
@@ -89,7 +138,7 @@ void iMouse(int button, int state, int mx, int my)
 		{
 			if(mx >= 50 && mx <= 250 && my >= 400 && my <= 468)
 			{
-                PlaySound("game.wav", NULL, SND_LOOP | SND_ASYNC);
+                PlaySound("bg music.wav", NULL, SND_LOOP | SND_ASYNC);
 
 			}
 			if(mx >= 550 && mx <= 750 && my >= 400 && my <= 468)
@@ -102,6 +151,14 @@ void iMouse(int button, int state, int mx, int my)
 		if(screen==3)
 		{
            if(mx >= 550 && mx <= 750 && my >= 30 && my <= 98)
+			{
+				screen=0;
+			}
+		}
+
+		if(screen == 4)
+		{
+			if(mx >= 550 && mx <= 750 && my >= 400 && my <= 468)
 			{
 				screen=0;
 			}
@@ -120,6 +177,16 @@ void iKeyboard(unsigned char key)
 		// do something with 'x'
 		exit(0);
 	}
+    if(screen!=1){
+
+	if(key == 'm'){
+	PlaySound("bg music.wav", NULL, SND_LOOP | SND_ASYNC);
+	}
+	if(key == 'o'){
+	  PlaySound(0,0,0);
+	}
+	}
+	
 
 	// place your codes for other keys here
 }
@@ -140,6 +207,8 @@ void iSpecialKeyboard(unsigned char key)
 	{
 		exit(0);
 	}
+	
+	
 
 	// place your codes for other keys here
 }
@@ -148,7 +217,7 @@ int main()
 	// place your own initialization codes here.
 	// t=iSetTimer(50,iChangePic);
 	
-	PlaySound("game.wav", NULL, SND_LOOP | SND_ASYNC);
+	PlaySound("bg music.wav", NULL, SND_LOOP | SND_ASYNC);
 	iInitialize(800, 500, "Pokemon Puzzle Game");
 	return 0;
 }
